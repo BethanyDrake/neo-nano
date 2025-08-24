20be88a0cdc1048ea317b7d9d43868a58b9d9370
Bethany Drake <bdrake@bigw.com>
2025-08-24 11:55:51 +1000

    format

--- packages/neo-nano-ui/src/pages/forum/[topic-id].tsx
+++ packages/neo-nano-ui/src/pages/forum/[topic-id].tsx
@@ -1,10 +1,10 @@
-import { ReturnType } from "@/app/api/threads/route"
-import { Thread } from "@/lib/forum.types"
-import { Column } from "@/lib/layout"
-import axios from "axios"
-import { NextPageContext } from "next"
-import { useCallback, useState } from "react"
-import { useForm, SubmitHandler } from "react-hook-form"
+import { ReturnType } from '@/app/api/threads/route'
+import { Thread } from '@/lib/forum.types'
+import { Column } from '@/lib/layout'
+import axios from 'axios'
+import { NextPageContext } from 'next'
+import { useCallback, useState } from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
 
 type Inputs = {
   title: string
@@ -12,16 +12,16 @@ type Inputs = {
 }
 
 type Topic = {
-    id: string
-    title: string
-    description: string
+  id: string
+  title: string
+  description: string
 }
 type ThreadSummary = {
-    id: string
-    title: string
+  id: string
+  title: string
 }
 
-const CreateThreadForm = ({topicId, onSubmit}: {topicId: string, onSubmit: () => void}) => {
+const CreateThreadForm = ({ topicId, onSubmit }: { topicId: string; onSubmit: () => void }) => {
   const {
     register,
     handleSubmit,
@@ -29,75 +29,72 @@ const CreateThreadForm = ({topicId, onSubmit}: {topicId: string, onSubmit: () =>
     formState: { errors },
   } = useForm<Inputs>()
 
-const _onSubmit: SubmitHandler<Inputs> = async (data: Inputs) => {
-  const body = {
-    ...data,
-    topic: topicId
+  const _onSubmit: SubmitHandler<Inputs> = async (data: Inputs) => {
+    const body = {
+      ...data,
+      topic: topicId,
+    }
+    await axios.post(`/api/threads`, body).then(() => {
+      reset()
+      onSubmit()
+    })
   }
-  await axios.post(`/api/threads`, body).then(() => {
-    reset()
-    onSubmit()
-})
 
-}
-
-
-     return (
+  return (
     <form onSubmit={handleSubmit(_onSubmit)}>
-        <Column>
-      <input placeholder="Thread Title" {...register("title")} required={true} />
+      <Column>
+        <input placeholder="Thread Title" {...register('title')} required={true} />
 
-      {errors.title && <span>This field is required</span>}
-      <input {...register("commentText", { required: true })} />
-      {errors.commentText && <span>This field is required</span>}
+        {errors.title && <span>This field is required</span>}
+        <input {...register('commentText', { required: true })} />
+        {errors.commentText && <span>This field is required</span>}
 
-      <input type="submit" />
+        <input type="submit" />
       </Column>
     </form>
   )
 }
 
+const TopicPage = ({ topic, initialThreads }: { topic: Topic; initialThreads: Thread[] }) => {
+  const [createThreadFormIsOpen, setCreateThreadFormIsOpen] = useState(false)
+  const [threads, setThreads] = useState<Thread[]>(initialThreads)
 
+  console.log({ threads })
+  const updateThreads = useCallback(async () => {
+    const _threads: Thread[] = (await axios.get<ReturnType>(`/api/threads?topic=${topic.id}`)).data.threads
+    console.log({ _threads })
+    setThreads(_threads)
+  }, [topic])
 
-const TopicPage = ({topic, initialThreads}:{topic: Topic, initialThreads:Thread[]}) => {
-    const [createThreadFormIsOpen, setCreateThreadFormIsOpen] = useState(false);
-    const [threads, setThreads] = useState<Thread[]>(initialThreads)
-
-    console.log({threads})
-    const updateThreads = useCallback(async () => {
-
-      const _threads: Thread[] = (await axios.get<ReturnType>(`/api/threads?topic=${topic.id}`)).data.threads
-        console.log({_threads})
-      setThreads(_threads)
-    }, [topic])
-
-    
-    return(<>
-        <div>{topic.id}</div>
-        {
-           threads && threads.map((thread:ThreadSummary) => {
-            return <div key={thread.id}>{thread.title}</div>
-           })
-        }
-        <button role="button" onClick={() => setCreateThreadFormIsOpen(true)}>Create Thread</button>
-        {createThreadFormIsOpen &&<CreateThreadForm onSubmit={updateThreads} topicId={topic.id} />}
-        </>)
+  return (
+    <>
+      <div>{topic.id}</div>
+      {threads &&
+        threads.map((thread: ThreadSummary) => {
+          return <div key={thread.id}>{thread.title}</div>
+        })}
+      <button role="button" onClick={() => setCreateThreadFormIsOpen(true)}>
+        Create Thread
+      </button>
+      {createThreadFormIsOpen && <CreateThreadForm onSubmit={updateThreads} topicId={topic.id} />}
+    </>
+  )
 }
 
-TopicPage.getInitialProps =  async (context: NextPageContext)  => {
-    const topicId = context.query["topic-id"] as string
-    console.log('url', `/api/threads?topic=${topicId}`)
-    const initialThreads = (await axios.get<ReturnType>(`${process.env.APP_BASE_URL}/api/threads?topic=${topicId}`)).data.threads
+TopicPage.getInitialProps = async (context: NextPageContext) => {
+  const topicId = context.query['topic-id'] as string
+  console.log('url', `/api/threads?topic=${topicId}`)
+  const initialThreads = (await axios.get<ReturnType>(`${process.env.APP_BASE_URL}/api/threads?topic=${topicId}`)).data
+    .threads
 
-    return {
-        topic: {
-            id: topicId,
-            title: "Topic Title",
-            description: "topic description"
-        },
-        initialThreads
-    }
-
+  return {
+    topic: {
+      id: topicId,
+      title: 'Topic Title',
+      description: 'topic description',
+    },
+    initialThreads,
+  }
 }
 
 export default TopicPage
--- packages/neo-nano-ui/src/pages/forum/index.tsx
+++ packages/neo-nano-ui/src/pages/forum/index.tsx
@@ -5,51 +5,64 @@ import * as Icons from '@fortawesome/free-solid-svg-icons'
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
 import { Category } from '@/lib/forum.types'
 
-const ForumItem = ({topicId, title, icon, description}: {topicId: string, title: string, icon: IconProp, description: string}) => {
-return( <div className={styles['forum-item']}>
-                    <Row justifyContent="left">
-                        
-                        <Column>
-                            <div className={styles["forum-icon"]}>
-                                <FontAwesomeIcon icon={icon} />
-                         </div>
-                         </Column>
-                         <Column>
-                            <a href={`forum/${topicId}`} className={styles["forum-item-title"]}>{title}</a>
-                            <div className={styles["forum-sub-title"]}>{description}</div>
-                        </Column>
-                       
-                </Row></div>)
+const ForumItem = ({
+  topicId,
+  title,
+  icon,
+  description,
+}: {
+  topicId: string
+  title: string
+  icon: IconProp
+  description: string
+}) => {
+  return (
+    <div className={styles['forum-item']}>
+      <Row justifyContent="left">
+        <Column>
+          <div className={styles['forum-icon']}>
+            <FontAwesomeIcon icon={icon} />
+          </div>
+        </Column>
+        <Column>
+          <a href={`forum/${topicId}`} className={styles['forum-item-title']}>
+            {title}
+          </a>
+          <div className={styles['forum-sub-title']}>{description}</div>
+        </Column>
+      </Row>
+    </div>
+  )
 }
-
 
 type ForumProps = {
- categories: Category[]
+  categories: Category[]
 }
-export const Forum = ({categories}: ForumProps) => {
+export const Forum = ({ categories }: ForumProps) => {
+  console.log('Forum prop', categories)
 
-    console.log("Forum prop", categories)
-
-    return (
+  return (
     <main>
-        <div className={styles['forum-container']}>
-{
-    categories.map(({id, title, topics }) => {
-        return (    <div key={id}><h2>{title}</h2>
-            {topics.map(({id, title, description, icon})=> {
-            return <ForumItem key={id} topicId={id} title={title} description={description} icon={Icons[icon]} />
-        })}</div>)
-    })
-}
-    
-</div></main>)
+      <div className={styles['forum-container']}>
+        {categories.map(({ id, title, topics }) => {
+          return (
+            <div key={id}>
+              <h2>{title}</h2>
+              {topics.map(({ id, title, description, icon }) => {
+                return <ForumItem key={id} topicId={id} title={title} description={description} icon={Icons[icon]} />
+              })}
+            </div>
+          )
+        })}
+      </div>
+    </main>
+  )
 }
 
 Forum.getInitialProps = async () => {
   const res = await fetch(`${process.env.APP_BASE_URL}/api/getTopics`)
-  const {categories} = await res.json()
+  const { categories } = await res.json()
   return { categories }
 }
- 
-export default Forum
 
+export default Forum
