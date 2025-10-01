a69f1e47435c2c44967a8751070ef7c1bbc07a4f
Bethany Drake <bdrake@bigw.com>
2025-10-02 07:41:03 +1000

    getIsModerator when no user is logged in.

--- packages/neo-nano-ui/src/lib/serverFunctions/moderation/getIsModerator.ts
+++ packages/neo-nano-ui/src/lib/serverFunctions/moderation/getIsModerator.ts
@@ -7,16 +7,23 @@ import { getSingle } from '../_utils/getSingle'
 
 export const getIsModerator = async () => {
   const session = await auth0.getSession()
+  if (!session) return false
   const sql = getQueryFunction()
   const external_id = session?.user.sub
+
   await sql`select role from users where external_id=${external_id}
   ;`
 
+  try {
   const { role } = (await getSingle(
     'user',
     sql`select role from users where external_id=${external_id}
   ;`,
   )) as Profile
 
-  return role === 'moderator'
+    return role === 'moderator'
+  } catch (error) {
+    console.error(error)
+    return false
+  }
 }
