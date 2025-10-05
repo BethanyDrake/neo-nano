b973421c1d4f3df30689bdb8b9b1dd021a73d6c5
Bethany Drake <bdrake@bigw.com>
2025-10-06 08:56:00 +1100

    public profile page doesn't require login

--- packages/neo-nano-ui/src/app/profile/[userId]/page.tsx
+++ packages/neo-nano-ui/src/app/profile/[userId]/page.tsx
@@ -1,17 +1,11 @@
-import { auth0 } from '@/lib/auth0'
 import { Goal, Profile } from '@/lib/forum.types'
 import { PublicGoalSection } from '@/lib/goalTracker/PublicGoalSection'
 import { Centered } from '@/lib/layout'
 import { getPublicGoals } from '@/lib/serverFunctions/goals/getPublicGoals'
 import { getPublicProfile } from '@/lib/serverFunctions/profile/publicProfile'
-import { redirect } from 'next/navigation'
+
 const PublicProfilePage = async ({ params }: { params: Promise<{ userId: string }> }) => {
   const userId = (await params).userId
-  const session = await auth0.getSession()
-  if (!session) {
-    return redirect(`${process.env.APP_BASE_URL}/auth/login?returnTo=${process.env.APP_BASE_URL}/profile`)
-  }
-
   const profile: Profile = await getPublicProfile(userId)
   const goals: Goal[] = await getPublicGoals(userId)
 
