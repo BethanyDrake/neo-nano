44ce931a870dca29b5c9ccd15cf64b0c18118578
Bethany <bethany.anne.drake@gmail.com>
2025-11-03 12:24:25 +1100

    include openGraph Metadata

--- packages/neo-nano-ui/src/app/layout.tsx
+++ packages/neo-nano-ui/src/app/layout.tsx
@@ -4,16 +4,23 @@ import { auth0 } from '@/lib/auth0'
 import { NavBar } from '@/lib/NavBar'
 // The following import prevents a Font Awesome icon server-side rendering bug,
 // where the icons flash from a very large icon down to a properly sized one:
-import '@fortawesome/fontawesome-svg-core/styles.css';
+import '@fortawesome/fontawesome-svg-core/styles.css'
 // Prevent fontawesome from adding its CSS since we did it manually above:
-import { config } from '@fortawesome/fontawesome-svg-core';
-import { getIsModerator } from '@/lib/serverFunctions/moderation/getIsModerator';
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/next"
-config.autoAddCss = false;  
+import { config } from '@fortawesome/fontawesome-svg-core'
+import { getIsModerator } from '@/lib/serverFunctions/moderation/getIsModerator'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/next'
+config.autoAddCss = false
 export const metadata: Metadata = {
   title: 'Novel November',
   description: 'Write a novel this November!',
+  openGraph: {
+    title: 'Write a novel this November!',
+    description: '30 days, 50K words. Can you beat the challenge?',
+    url: 'https://www.novel-november.com',
+    siteName: 'Novel November',
+    images: [{ url: 'https://www.novel-november.com/icon0.svg' }],
+  },
 }
 
 export default async function RootLayout({
@@ -26,12 +33,12 @@ export default async function RootLayout({
   const isLoggedIn = !!session
   return (
     <html lang="en">
-      <head/>
+      <head />
 
       <body>
         <NavBar isLoading={false} isLoggedIn={!!isLoggedIn} isModerator={isModerator} />
         <main>{children}</main>
-        <SpeedInsights/>
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
