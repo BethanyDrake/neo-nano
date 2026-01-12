7bb6d76c6bf0b86d8f32030b22b8fd7143dfca3a
Bethany <bethany.anne.drake@gmail.com>
2026-01-13 08:45:47 +1100

    simple timer

---
+++ packages/neo-nano-ui/src/app/tools/timer/page.tsx
@@ -0,0 +1,12 @@
+import { GutteredPage } from '@/lib/layoutElements/GutteredPage'
+import { Timer } from '@/lib/tools/timer'
+
+const TimerPage = () => {
+  return (
+    <GutteredPage>
+      <Timer />
+    </GutteredPage>
+  )
+}
+
+export default TimerPage
