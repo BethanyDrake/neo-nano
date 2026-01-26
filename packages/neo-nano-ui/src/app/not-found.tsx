f76944c938990e34f7f181d41845db7b9263319a
Bethany <bethany.anne.drake@gmail.com>
2026-01-27 06:58:28 +1100

    fix spacing on 404 page

--- packages/neo-nano-ui/src/app/not-found.tsx
+++ packages/neo-nano-ui/src/app/not-found.tsx
@@ -1,12 +1,14 @@
 import { Column } from "@/lib/layoutElements/flexLayouts"
+import { FullWidthPage } from "@/lib/layoutElements/FullWidthPage"
 import Image from "next/image"
 
 function NotFoundPage() {
 
 	return (
+		<FullWidthPage>
 		<Column alignItems="center" style={{margin: '2em auto'}}>
 	<Image width={450} height={175} src="https://ytw3r4gan2ohteli.public.blob.vercel-storage.com/not-found.svg" alt={"Broken pencil, representing sadness."}/>
-<h1>Page Not Found</h1><p>This page does not exist. Care to try another?</p></Column>)
+<h1>Page Not Found</h1><p>This page does not exist. Care to try another?</p></Column></FullWidthPage>)
 }
 
 export default NotFoundPage
