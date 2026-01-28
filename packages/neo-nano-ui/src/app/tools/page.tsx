0b6e301e5b997fb98ef578f47fa7f0a8463a2e9e
Bethany <bethany.anne.drake@gmail.com>
2026-01-28 18:25:25 +1100

    style tool descriptions

--- packages/neo-nano-ui/src/app/tools/page.tsx
+++ packages/neo-nano-ui/src/app/tools/page.tsx
@@ -9,7 +9,7 @@ import focusClock from '@/lib/tools/images/focus-clock.png'
 import sprintTimer from '@/lib/tools/images/sprint-timer.png'
 import { StaticImport } from 'next/dist/shared/lib/get-img-props'
 
-const ToolCard = ({toolName, imageSrc, id}: {id: string, toolName: string, imageSrc: string | StaticImport} ) => {
+const ToolCard = ({toolName, imageSrc, id, description}: {id: string, toolName: string, imageSrc: string | StaticImport, description: string} ) => {
     return (
 
     <div className={classNames.card}><Link style={{width: '100%'}}href={`/tools/${id}`}><h2>{toolName}</h2> <Image
@@ -17,7 +17,7 @@ const ToolCard = ({toolName, imageSrc, id}: {id: string, toolName: string, image
               width={100}
               height={100}
               src={imageSrc}
-            /></Link></div>
+            /><div className={classNames.description}>{description}</div></Link></div>
     )
 }
 
@@ -25,11 +25,11 @@ const ToolsPage = () => {
   return (
     <NewAwardModalProvider >
     <GutteredPage>
-      <UnderDevelopmentMessage upcomingChanges={[]}/>
+      <UnderDevelopmentMessage upcomingChanges={['enter word count after a sprint', 'graph wpm over a sprint session']}/>
      <Centered><h1>Toolbox</h1></Centered>
       <div className={classNames.container}>
-      <ToolCard  id={"timer"} toolName="Sprint timer" imageSrc={sprintTimer}/>
-      <ToolCard id={"focus-clock"} toolName="Focus clock" imageSrc={focusClock}/>
+      <ToolCard  description={"Set a timer, then see how many words you can write!"}id={"timer"} toolName="Sprint timer" imageSrc={sprintTimer}/>
+      <ToolCard description={'Just start editing, and log the time after.'} id={"focus-clock"} toolName="Focus clock" imageSrc={focusClock}/>
       </div>
     </GutteredPage>
     <NewAwardModal/>
