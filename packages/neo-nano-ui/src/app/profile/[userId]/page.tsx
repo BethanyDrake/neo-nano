0da669b1dcb4257149d9cb0859c787538b534ad1
Bethany <bethany.anne.drake@gmail.com>
2026-02-15 08:05:59 +1100

    style public profile with mui grid

--- packages/neo-nano-ui/src/app/profile/[userId]/page.tsx
+++ packages/neo-nano-ui/src/app/profile/[userId]/page.tsx
@@ -1,5 +1,5 @@
 import { PublicGoalSection } from '@/lib/goalTracker/PublicGoalSection'
-import { Centered } from '@/lib/layoutElements/flexLayouts'
+import { Centered, Column } from '@/lib/layoutElements/flexLayouts'
 import { getPublicGoals } from '@/lib/serverFunctions/goals/getPublicGoals'
 import { getPublicProfile } from '@/lib/serverFunctions/profile/publicProfile'
 import { TrophyCase } from '@/lib/awards/TrophyCase'
@@ -7,6 +7,11 @@ import { getPublicAwards } from '@/lib/serverFunctions/awards/getPublicAwards'
 import { FullWidthPage } from '@/lib/layoutElements/FullWidthPage'
 import { getPublicProjects } from '@/lib/serverFunctions/projects/getPublicProjects'
 import { PublicProjectSection } from '@/lib/projects/PublicProjectSection'
+import { PropsWithChildren } from 'react'
+
+const Header = ({children}: PropsWithChildren) => {
+  return (<Centered><h2 style={{fontSize: '2em'}}>{children}</h2></Centered>)
+}
 
 const PublicProfilePage = async ({ params }: { params: Promise<{ userId: string }> }) => {
   const userId = (await params).userId
@@ -14,21 +19,26 @@ const PublicProfilePage = async ({ params }: { params: Promise<{ userId: string 
 
   return (
     <FullWidthPage>
+      <Column>
       <Centered>
         <h1>{profile.displayName}&apos;s Profile</h1>
       </Centered>
+      
       <h2>{profile.displayName}</h2>
       {profile.role === 'moderator' && <p>Moderator</p>}
       <p style={{ whiteSpaceCollapse: 'preserve' }}>{profile.aboutMe}</p>
         
       <TrophyCase awards={awards}/>
+         <Header>{profile.displayName}&apos;s Novels</Header>
       {projects.map((project) => (
         <PublicProjectSection key={project.id} project={project} />
       ))}
+
+      <Header>{profile.displayName}&apos;s Goals</Header>
       {goals.map((goal) => (
         <PublicGoalSection key={goal.id} goal={goal} />
       ))}
-     
+     </Column>
     </FullWidthPage>
   )
 }
--- packages/neo-nano-ui/src/lib/projects/PublicProjectSection.tsx
+++ packages/neo-nano-ui/src/lib/projects/PublicProjectSection.tsx
@@ -1,33 +1,47 @@
 import { Project } from './Project.type'
 import { LeftRow } from '../layoutElements/flexLayouts'
+import novelCover from '@/lib/media/novel-cover.png'
+import Grid from '@mui/material/Grid';
 
+import Image from 'next/image'
 export const PublicProjectSection = ({ project }: { project: Project }) => {
   const { title, blurb, status, excerpt, wordCount} = project
   return (
-    <>
+    <div>
       <LeftRow>
-        <h3 style={{fontSize: '1.5em'}}>{title}</h3>
-        <span style={{ color: 'var(--text-colour-2)', fontSize: '1.5em' }}>({status})</span>
+        <h3 style={{fontSize: '2em'}}>{title}</h3>
       </LeftRow>
 
+      <Grid container spacing={3}>
+   
       {blurb && (
-        <>
+        <Grid size={{sm: 12, md: 6}}>
           <h4>Blurb:</h4>
           <p style={{ whiteSpaceCollapse: 'preserve' }}>{blurb}</p>
-        </>
+        </Grid>
       )}
+
+   <Grid size={{sm: 12, md: 6}}>
+      <Image style={{borderRadius: '4px', boxShadow: "var(--shadow-1)"}} height="300" src={novelCover} alt={title}/>
+      {wordCount &&<LeftRow>
+        <h4>Word count:</h4> <span>{wordCount / 1000}K</span>
+      </LeftRow> }
+        <LeftRow><h4>Status:</h4> <span>{status}</span></LeftRow>
+</Grid>
       {excerpt && (
-        <>
+        <Grid size={{sm: 12, md: 6}}>
           <h4>Excerpt:</h4>
           <p style={{ whiteSpaceCollapse: 'preserve' }}>{excerpt}</p>
-        </>
+        </Grid>
       )}
 
       {wordCount && (
-        <LeftRow>
-          <h4>Expected/Final word count:</h4> <span>{wordCount / 1000}K</span>
-        </LeftRow>
+        <Grid size={6} >
+       
+        </Grid>
       )}
-    </>
+
+       </Grid>
+    </div>
   )
 }
