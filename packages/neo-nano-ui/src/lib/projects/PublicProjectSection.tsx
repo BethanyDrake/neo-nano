b9c0a78496d33628e91ff1af7253e00cb3ff50d7
Bethany <bethany.anne.drake@gmail.com>
2026-02-21 15:23:06 +1100

    style public project

--- packages/neo-nano-ui/src/lib/projects/PublicProjectSection.tsx
+++ packages/neo-nano-ui/src/lib/projects/PublicProjectSection.tsx
@@ -1,49 +1,32 @@
 import { Project } from './Project.type'
-import { LeftRow } from '../layoutElements/flexLayouts'
-import novelCover from '@/lib/media/novel-cover.png'
-import Grid from '@mui/material/Grid';
-
-import Image from 'next/image'
 import { AspectChart } from './AspectChart';
+import { Column, LeftRow } from '../layoutElements/flexLayouts';
+
 export const PublicProjectSection = ({ project }: { project: Project }) => {
   const { title, blurb, status, excerpt, wordCount} = project
   return (
-    <div>
-      <LeftRow>
-        <h3 style={{fontSize: '2em'}}>{title}</h3>
-      </LeftRow>
-
-      <Grid container spacing={3}>
+    <div style={{display: 'flex', flexWrap: 'wrap', alignItems: 'start', justifyContent: 'center', gap: '32px'}}>
    
-      {blurb && (
-        <Grid size={{sm: 12, md: 6}}>
+        <div style={{maxWidth: '500px'}}><Column>
+          <h3 style={{fontSize: '2em'}}>{title}</h3>
+         {blurb && (
+        <>
           <h4>Blurb:</h4>
-          <p style={{ whiteSpaceCollapse: 'preserve' }}>{blurb}</p>
-        </Grid>
-      )}
-
-   <Grid size={{sm: 12, md: 6}}>
-      <Image style={{borderRadius: '4px', boxShadow: "var(--shadow-1)"}} height="300" src={novelCover} alt={title}/>
-       <AspectChart aspects={project.aspects}/>
-      {wordCount &&<LeftRow>
-        <h4>Word count:</h4> <span>{wordCount / 1000}K</span>
-      </LeftRow> }
-        <LeftRow><h4>Status:</h4> <span>{status}</span></LeftRow>
-</Grid>
-      {excerpt && (
-        <Grid size={{sm: 12, md: 6}}>
+          <p style={{ whiteSpaceCollapse: 'preserve' }}>{blurb}</p></>)}
+     {excerpt && (
+        <>
           <h4>Excerpt:</h4>
           <p style={{ whiteSpaceCollapse: 'preserve' }}>{excerpt}</p>
-        </Grid>
+        </>
       )}
+      </Column></div>
+      <Column><AspectChart aspects={project.aspects}/>{wordCount &&<>
+        <LeftRow><h4>Word count:</h4> <span>{wordCount / 1000}K</span></LeftRow>
+       <LeftRow> <h4>Status:</h4> <span>{status}</span></LeftRow>
+      </> }</Column>
+     
 
-      {wordCount && (
-        <Grid size={6} >
-       
-        </Grid>
-      )}
-
-       </Grid>
+  
     </div>
   )
 }
