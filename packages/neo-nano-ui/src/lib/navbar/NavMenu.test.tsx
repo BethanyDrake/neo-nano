69ae1f127b3887b86792bb285bb2f2eb44f41751
Bethany <bethany.anne.drake@gmail.com>
2026-04-12 17:32:54 +1000

    update vitest

--- packages/neo-nano-ui/src/lib/navbar/NavMenu.test.tsx
+++ packages/neo-nano-ui/src/lib/navbar/NavMenu.test.tsx
@@ -7,22 +7,19 @@ import { withReactQueryClient } from '@/tests/utils/withReactQueryClient'
 vi.mock('@/lib/serverFunctions/moderation/getIsModerator')
 
 describe('<NavMenu />', () => {
+  test('regular user', async () => {
+    vi.mocked(getIsModerator).mockResolvedValue(false)
+    const { findByRole, getByRole } = render(<NavMenu />, { wrapper: wrap(withReactQueryClient()) })
+    expect(getByRole('button'))
+    fireEvent.click(getByRole('button'))
+    await findByRole('menuitem', { name: 'Home' })
+  })
+  test('moderator', async () => {
+    vi.mocked(getIsModerator).mockResolvedValue(true)
+    const { findByRole, getByRole } = render(<NavMenu />, { wrapper: wrap(withReactQueryClient()) })
+    expect(getByRole('button'))
+    fireEvent.click(getByRole('button'))
 
-        test('regular user', async () => {  
-        vi.mocked(getIsModerator).mockResolvedValue(false)
-        const {findByRole, getByRole } = render(<NavMenu />, {wrapper: wrap(withReactQueryClient())})
-        expect(getByRole('button'))
-        fireEvent.click(getByRole('button'))
-        await findByRole('menuitem', {name: 'Home'})
-
-    })
-    test('moderator', async () => {
-        vi.mocked(getIsModerator).mockResolvedValue(true)
-        const {findByRole, getByRole,  } = render(<NavMenu />, {wrapper: wrap(withReactQueryClient())})
-        expect(getByRole('button'))
-        fireEvent.click(getByRole('button'))
-            
-        await findByRole('menuitem', {name: 'Moderation'})
-
-    })
+    await findByRole('menuitem', { name: 'Moderation' })
+  })
 })
