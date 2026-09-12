be122e1fa2d4f20d4034b179af94a8d1a41f7787
Bethany <bethany.anne.drake@gmail.com>
2026-09-12 19:43:07 +1000

    Revert "specify db connection timeout"
    This reverts commit 163b9b99fb279ed22ae6c9ca2a88806d05c7f52d.

--- packages/neo-nano-ui/src/lib/serverFunctions/_utils/getDbConnection.ts
+++ packages/neo-nano-ui/src/lib/serverFunctions/_utils/getDbConnection.ts
@@ -1,9 +1,7 @@
 import { neon } from '@neondatabase/serverless'
 
-const DB_TIMEOUT_MS = 10_000
-
 export const getDbConnection = () => {
   if (!process.env.DATABASE_URL) throw Error('DATABASE_URL not defined.')
-  const sql = neon(process.env.DATABASE_URL, {fetchOptions: { signal: AbortSignal.timeout(DB_TIMEOUT_MS) }})
+  const sql = neon(process.env.DATABASE_URL)
   return sql
 }
